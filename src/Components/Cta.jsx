4b84de664355b26c3de27e8018274ce9d0e9e709
julius-tamale <juliotamex@proton.me>
2024-04-24 12:33:10 +0300

    bitebytes fast food website template project

---
+++ src/Components/Cta.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cta() {
+  return (
+    <div>
+      CTA
+    </div>
+  )
+}
+
+export default Cta
---
+++ src/Components/Main.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Hero from './Hero'
+import Promo from './Promo'
+import About from './About'
+import FoodMenu from './FoodMenu'
+import Cta from './Cta'
+import Delivery from './Delivery'
+import Testimonials from './Testimonials'
+import Banner from './Banner'
+import Blog from './Blog'
+
+
+
+function Main() {
+  return (
+    <div>
+      <Hero />
+      <Promo />
+      <About />
+      <FoodMenu/>
+      <Cta />
+      <Delivery />
+      <Testimonials />
+      <Banner />
+      <Blog />
+    </div>
+  )
+}
+
+export default Main
